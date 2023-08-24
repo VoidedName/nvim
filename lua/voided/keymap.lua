57881e3a3c9a66f3c44d43b4c2d351f4f7e26381
Daniel Kogan <daniel.kogan@materna.group>
2023-08-24 12:35:56 +0200

    some more configs

--- lua/voided/keymap.lua
+++ lua/voided/keymap.lua
@@ -27,4 +27,5 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set('n', '<C-Up>', ':resize +2<CR>')
 vim.keymap.set('n', '<C-Down>', ':resize -2<CR>')
 vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
 
